b8706afa7f2959dc59bd57f52b7196224066e067
Dave <dbeals85@gmail.com>
2017-09-26 20:40:09 -0700

    adding script file and lightbox

---
+++ script/script.js
@@ -0,0 +1,12 @@
+console.log("The JavaScript works! I AM ALIVE!")
+
+let correct = "Correct!"
+let incorrectFalse = "True!"
+let incorrectTrue = "False!"
+let answer7 = "This is a law in Lynden, WA."
+let answer9 = "This is a law in Alabama."
+let answer10 = "This is a law in New Orleans."
+
+function buildAnswers() {
+  if(document.getElementById)
+}
